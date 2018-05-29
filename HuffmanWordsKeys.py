c7c85b1c1c6e4adf31728a4532ee2a9e07f368ee
Miha Tomšič <5785564+MihaTomsic@users.noreply.github.com>
2018-05-29 13:59:45 +0200

    Added apps for actual bitcoin key creation

---
+++ HuffmanWordsKeys.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from huffman import Huffman			
+from bitstring import Bits, BitArray
+import keyUtils3
+
+if len(sys.argv) == 2:
+	t0 = sys.argv[1]
+else:  
+	t0 = "1234567890 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLNOPQRSTUVWXYZ" 
+
+words = t0.split()
+wordcount = len(words)    
+lens = map(len,words)
+maxlen = max(lens)
+
+string1 = ""
+for c in range(maxlen):
+    for w in words:
+        if c < len(w):
+            string1 += w[c]
+
+string2 = "".join(words)
+    
+string = string1 + string2    
+print(string)    
+huff = Huffman(string)  
+huff.huffman_code()
+b = huff.huffman_encode()
+
+if len(b) < 256:
+	print("Not enough bits of information for private key: ", len(b) ,"< 256 bits")
+	sys.exit(1)
+
+# take first 256 bits for private key
+priv = b[:256].hex	  # hex private key
+print("Priv:", priv)
+
+wif = keyUtils3.privateKeyToWif(priv)
+print("WIF:", wif)
+
+pub = keyUtils3.privateKeyToPublicKey(priv)
+print("Public:", pub)
+
+addr = keyUtils3.keyToAddr(priv)
+print("Address:", addr)
---
+++ utils3.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import hashlib
+import struct
+import unittest
+
+b58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+b58b = b58.encode()
+
+# Returns byte string value, not hex string
+def varint(n):
+    if n < 0xfd:
+        return struct.pack('<B', n)
+    elif n < 0xffff:
+        return struct.pack('<cH', b'\xfd', n)
+    elif n < 0xffffffff:
+        return struct.pack('<cL', b'\xfe', n)
+    else:
+        return struct.pack('<cQ', b'\xff', n)
+
+# Takes and returns byte string value, not hex string
+def varstr(b):
+    #s = s.decode("ascii")
+    return varint(len(b)) + b
+
+# 60002
+def netaddr(ipaddr, port):
+    services = 1
+    return (struct.pack('<Q12s', services, '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff') +
+                       struct.pack('>4sH', ipaddr, port))
+
+# return value, len, payload is bytes
+def processVarInt(payload):
+    n0 = payload[0]
+    if n0 < 0xfd:
+        return [n0, 1]
+    elif n0 == 0xfd:
+        return [struct.unpack('<H', payload[1:3])[0], 3]
+    elif n0 == 0xfe:
+        return [struct.unpack('<L', payload[1:5])[0], 5]
+    else:
+        return [struct.unpack('<Q', payload[1:5])[0], 7]
+
+# return value, len, payload in bytes
+def processVarStr(payload):
+    n, length = processVarInt(payload)
+    return [payload[length:length+n], length + n]
+
+# takes 26 byte input, returns string  
+def processAddr(payload):
+    assert(len(payload) >= 26)
+    return '%d.%d.%d.%d:%d' % (payload[20], payload[21],
+                               payload[22], payload[23],
+                               struct.unpack('!H', payload[24:26])[0])
+
+
+def base58encode(n):
+    result = b''
+    while n > 0:
+        result = b58[n%58].encode() + result
+        n //= 58
+    return result
+
+def base58decode(b):
+    if type(b) == str:
+        b = b.encode()
+    result = 0
+    for i in range(0, len(b)):
+        result = result * 58 + b58b.index(b[i])
+    return result
+
+def base256encode(n):
+    result = b''
+    while n > 0:
+        result = bytes((n % 256,)) + result
+        n //= 256
+    return result
+
+def base256decode(b):
+    result = 0
+    # byte is split into integers   
+    for i in b:
+        result = result * 256 + i
+    return result
+
+def countLeadingChars(s, ch):
+    count = 0
+    for c in s:
+        if c == ch:
+            count += 1
+        else:
+            break
+    return count
+
+# https://en.bitcoin.it/wiki/Base58Check_encoding
+def base58CheckEncode(version, payload):
+    b = bytes((version,)) + payload
+    b0 = hashlib.sha256(b).digest()
+    checksum = hashlib.sha256(b0).digest()[0:4]
+    result = b + checksum
+    leadingZeros = countLeadingChars(result, 0)
+    return b'1' * leadingZeros + base58encode(base256decode(result))
+
+def base58CheckDecode(b):
+    leadingOnes = countLeadingChars(b, 49)
+    b0 = base58decode(b)
+    b = base256encode(b0)
+    result = b'\x00' * leadingOnes + b[:-4]
+    chk = b[-4:]
+    checksum = hashlib.sha256(hashlib.sha256(result).digest()).digest()[0:4]
+    assert(chk == checksum)
+    version = result[0]
+    return result[1:]
+
+class TestUtils(unittest.TestCase):
+    def test_varint(self):
+        self.assertEqual(varint(0x42), b'\x42')
+        self.assertEqual(varint(0x123), b'\xfd\x23\x01')
+        self.assertEqual(varint(0x12345678), b'\xfe\x78\x56\x34\x12')
+        self.assertEqual(processVarInt(varint(0x42)), [0x42, 1])
+        self.assertEqual(processVarInt(varint(0x1234)), [0x1234, 3])
+
+    def test_varstr(self):
+        self.assertEqual(varstr(b'abc'), b'\x03abc')
+        self.assertEqual(processVarStr(b'\x03abc'), [b'abc', 4])
+
+    def test_processAddr(self):
+        self.assertEqual(processAddr(b'x'*20 + b'\x62\x91\x98\x16\x20\x8d'),
+                         '98.145.152.22:8333')
+
+    def test_countLeadingCharacters(self):
+        self.assertEqual(countLeadingChars('a\0bcd\0', '\0'), 0)
+        self.assertEqual(countLeadingChars('\0\0a\0bcd\0', '\0'), 2)        
+        self.assertEqual(countLeadingChars('1a\0bcd\0', '1'), 1)
+
+    def test_base256(self):
+        self.assertEqual(base256encode(base256decode(b'abc')), b'abc')
+        self.assertEqual(base256encode(0x4142), b'AB')
+        self.assertEqual(base256decode(b'AB'), 0x4142)
+
+    def test_base58(self):
+        self.assertEqual(base58encode(base58decode(b'abc')), b'abc')
+        self.assertEqual(base58decode(b'121'), 58)
+        self.assertEqual(base58decode(b'5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ'),
+            0x800C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D507A5B8D)
+
+    def test_base58check(self):
+        self.assertEqual(base58CheckDecode(base58CheckEncode(42, b'abc')), b'abc')
+        self.assertEqual(base58CheckDecode(base58CheckEncode(0, b'\0\0abc')), b'\0\0abc')
+        s = base256encode(0x0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D)
+        b = base58CheckEncode(0x80, s)
+        self.assertEqual(b, b"5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ")
+
+if __name__ == '__main__':
+    unittest.main()
